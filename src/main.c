822b6772d42667b933ac3c4a2d94384c4d6e9aff
Bang Corrupt <bangcorrupt@gmail.com>
2021-07-02 13:28:57 +0100

    delay function

--- src/main.c
+++ src/main.c
@@ -2,7 +2,8 @@
 
 #include <stm32f411xe.h>
 
-int blink();
+void blink();
+void delay(int d);
 
 int main() {
     // Enable GPIOC Clock
@@ -23,25 +24,25 @@ int main() {
 }
 
 
-int blink() {
+void blink() {
         
-        int delay = 0x100000;
+        int d = 0x100000;
         
         // Set GPIO_13
         GPIOC->BSRR = GPIO_BSRR_BS13;
         
-        // Wait a while    
-        for (int i = 0; i < delay; i++) {
-            __asm__("nop");
-        }
+        delay(d);
         
         // Clear GPIO_13
         GPIOC->BSRR = GPIO_BSRR_BR13;
         
-        // Wait a while
-        for (int i = 0; i < delay; i++) {
-            __asm__("nop");
-        }
-        
-        return 0;
+        delay(d);
+}
+
+void delay(int d) {
+    
+    // Wait a while    
+    for (int i = 0; i < d; i++) {
+        __asm__("nop");
+    }
 }
